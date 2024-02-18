bc70d8d198dac3459657275afd1b7275fcd88181
SzymCode <s.radomski19@gmail.com>
2024-02-18 21:32:24 +0100

    Recursive function loadTemplates to load templates ony by one

--- src/js/loadTemplates.js
+++ src/js/loadTemplates.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
-    function loadTemplate(templateName) {
-
-        $.ajax({
-            url: `${templateName}.html`,
-            method: 'GET',
-            success: function (data) {
-                $('#app').html(data);
-                console.log(`Template: ${templateName} has been loaded successfully!`)
-            },
-            error: function () {
-                $('#app').html('<p>Error loading template.</p>');
-                console.log('Error loading template.')
+    function loadTemplates(templateNames) {
+        // Recursive function to load templates one by one
+        function loadTemplate(index) {
+            if (index < templateNames.length) {
+                const templateName = templateNames[index];
+                $.ajax({
+                    url: `${templateName}.html`,
+                    method: 'GET',
+                    success: function (data) {
+                        $('#app').append(data);
+                        console.log(`${index + 1}. Template: ${templateName}.html has been loaded successfully!`);
+                        loadTemplate(index + 1); // Load next template recursively
+                    },
+                    error: function () {
+                        $('#app').append('<p>Error loading template.</p>');
+                        console.log('Error loading template.');
+                    }
+                });
             }
-        });
+        }
+
+        // Start loading templates
+        loadTemplate(0);
     }
 
-    loadTemplate('search');
+    loadTemplates(['search']);
 });
